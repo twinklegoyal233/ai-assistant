e8b9a530e806509d2942906687b3c440cc2aa25e
twinklegoyal233 <twinklegoyal420@gmail.com>
2024-06-19 16:05:20 +0530

    api bug fix

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,7 @@ import {
   const MODEL_NAME = "gemini-1.0-pro";
   
   // Paste Your API KEY Below
-  const API_KEY = "Paste Your API KEY Here";
+  const API_KEY = "AIzaSyCMV3ny-ISx9qhtgH-INh7w6yxuY9y1lj4";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
